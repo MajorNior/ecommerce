23ee0267a425cef677e4464eb4494790e541f9a0
leonardo <leonardomajornior@gmail.com>
2022-04-29 07:44:39 -0300

    projeto finalizado

---
+++ views-cache/index.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -0,0 +1,23 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Bem vindo
+        <small>Alfa Prog E-commerce</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="/admin"><i class="fa fa-dashboard"></i>home</a></li>
+        <li class="active">seguinte</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Your Page Content Here -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
